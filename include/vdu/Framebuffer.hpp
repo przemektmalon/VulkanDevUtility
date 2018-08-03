52c31e50e27d78b21c8caeaca6c072d4ffd57dd1
Przemek Malon <przemekmalon@hotmail.com>
2018-08-03 01:17:48 +0100

    Framebuffers

---
+++ include/vdu/Framebuffer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "PCH.hpp"
+#include "LogicalDevice.hpp"
+#include "DeviceMemory.hpp"
+#include "RenderPass.hpp"
+
+namespace vdu
+{
+	class Framebuffer
+	{
+	public:
+		Framebuffer() {}
+
+		void addAttachment(vdu::Texture* att, std::string name);
+
+		void create(vdu::LogicalDevice* device, vdu::RenderPass* renderPass);
+
+		void destroy();
+
+		const VkFramebuffer& getHandle() { return m_framebuffer; }
+
+	private:
+
+		VkFramebuffer m_framebuffer;
+
+		vdu::RenderPass* m_renderPass;
+
+		vdu::LogicalDevice* m_logicalDevice;
+
+		std::vector<vdu::Texture*> m_attachments;
+		std::vector<std::string> m_attachmentNames;
+	};
+}
---
+++ src/Framebuffer.cpp
@@ -0,0 +1,40 @@
+#include "PCH.hpp"
+#include "Framebuffer.hpp"
+
+void vdu::Framebuffer::addAttachment(vdu::Texture * att, std::string name)
+{
+	m_attachments.push_back(att);
+	if (name.length() == 0)
+		m_attachmentNames.push_back("Attachment " + std::to_string(m_attachments.size()));
+	else
+		m_attachmentNames.push_back(name);
+}
+
+void vdu::Framebuffer::create(vdu::LogicalDevice * device, vdu::RenderPass * renderPass)
+{
+	m_logicalDevice = device;
+	m_renderPass = renderPass;
+
+	std::vector<VkImageView> attachmentViews;
+
+	for (auto att : m_attachments)
+	{
+		attachmentViews.push_back(att->getView());
+	}
+
+	VkFramebufferCreateInfo framebufferInfo = {};
+	framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+	framebufferInfo.renderPass = m_renderPass->getHandle();;
+	framebufferInfo.attachmentCount = static_cast<uint32_t>(attachmentViews.size());
+	framebufferInfo.pAttachments = attachmentViews.data();
+	framebufferInfo.width = m_attachments[0]->getWidth();
+	framebufferInfo.height = m_attachments[0]->getHeight();
+	framebufferInfo.layers = 1;
+
+	VDU_VK_CHECK_RESULT(vkCreateFramebuffer(m_logicalDevice->getHandle(), &framebufferInfo, nullptr, &m_framebuffer));
+}
+
+void vdu::Framebuffer::destroy()
+{
+	VDU_VK_VALIDATE(vkDestroyFramebuffer(m_logicalDevice->getHandle(), m_framebuffer, 0));
+}
